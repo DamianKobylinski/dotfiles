592b083a5b8cf636895e8be5c83a1bbe168b4f6c
Damian Kobyliński <damiankob12@gmail.com>
2025-07-21 00:12:29 +0200

    feat: nvim update

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -4,6 +4,36 @@ return {
 	lazy = false,
 	---@type snacks.Config
 	opts = {
+		dashboard = {
+			preset = {
+				pick = function(cmd, opts)
+					return LazyVim.pick(cmd, opts)()
+				end,
+				header = [[
+  /$$$$$$  /$$   /$$ /$$$$$$$$ /$$$$$$ /$$    /$$ /$$$$$$ /$$$$$$$  /$$$$$$$$ /$$$$$$ /$$$$$$$  /$$$$$$$$
+ /$$__  $$| $$$ | $$|__  $$__/|_  $$_/| $$   | $$|_  $$_/| $$__  $$| $$_____/|_  $$_/| $$__  $$| $$_____/
+| $$  \ $$| $$$$| $$   | $$     | $$  | $$   | $$  | $$  | $$  \ $$| $$        | $$  | $$  \ $$| $$      
+| $$$$$$$$| $$ $$ $$   | $$     | $$  |  $$ / $$/  | $$  | $$$$$$$ | $$$$$     | $$  | $$  | $$| $$$$$   
+| $$__  $$| $$  $$$$   | $$     | $$   \  $$ $$/   | $$  | $$__  $$| $$__/     | $$  | $$  | $$| $$__/   
+| $$  | $$| $$\  $$$   | $$     | $$    \  $$$/    | $$  | $$  \ $$| $$        | $$  | $$  | $$| $$      
+| $$  | $$| $$ \  $$   | $$    /$$$$$$   \  $/    /$$$$$$| $$$$$$$/| $$$$$$$$ /$$$$$$| $$$$$$$/| $$$$$$$$
+|__/  |__/|__/  \__/   |__/   |______/    \_/    |______/|_______/ |________/|______/|_______/ |________/
+]],
+        -- stylua: ignore
+        ---@type snacks.dashboard.Item[]
+        keys = {
+          { icon = "🪟", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+          { icon = "🧻", key = "n", desc = "New File", action = ":ene | startinsert" },
+          { icon = "🪧", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+          { icon = "📋", key = "p", desc = "Projects", action = ":lua Snacks.dashboard.pick('projects')" },
+          { icon = "⚙️", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+          { icon = "🏡", key = "s", desc = "Restore Session", section = "session" },
+          { icon = "🧪", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
+          { icon = "⛏️", key = "l", desc = "Lazy", action = ":Lazy" },
+          { icon = "🚪", key = "q", desc = "Quit", action = ":qa" },
+        },
+			},
+		},
 		picker = {
 			sources = {
 				explorer = {
