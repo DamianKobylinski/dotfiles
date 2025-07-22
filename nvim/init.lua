6a743cce0c52c2e19d62c8985a4e1cd97ff6e51f
Damian Kobyliński <damiankob12@gmail.com>
2025-07-22 14:25:38 +0200

    chore: add mini.nvim modules

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,10 +4,3 @@ vim.opt.termguicolors = true
 
 vim.opt.number = true
 vim.opt.relativenumber = true
-
-local map = vim.keymap.set
-
-map("n", "<A-j>", ":m .+1<CR>==")
-map("n", "<A-k>", ":m .-2<CR>==")
-map("v", "<A-j>", ":m '>+1<CR>gv=gv")
-map("v", "<A-k>", ":m '<-2<CR>gv=gv")
